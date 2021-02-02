f3702e4d7e01a4049574c77555bfe4c5fa0adbe2
Amal Damodar <38078222+AmalDamodar@users.noreply.github.com>
2021-02-02 12:11:11 +0530

    Updated Changes

--- Php Connect/insertform.php
+++ Php Connect/insertform.php
@@ -14,4 +14,17 @@ $gen=$_POST['gender'];
 $em=$POST['email'];
 $ph1=$_POST['code'];
 $ph2=$_POST[phno'];
-$sql="INSERT INTO formtble(Name,Password,Gender,Email,Phonenumber1,Phonenumber2)VALUES('$fname,'$pass','$gen','$em','$ph1',
+$sql="INSERT INTO formtble(Name,Password,Gender,Email,Phonenumber1,Phonenumber2)VALUES('$fname,'$pass','$gen','$em','$ph1','$ph2')";
+if(mysqli_query($conn,$sql)) {
+  echo "New Record Created Successfully !<br>";
+}
+else
+{
+echo "error: ".$ mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
+<br><br><input type="submit" value=" Back to homepage " name="submit">
+</center>
+</body>
+</html>
